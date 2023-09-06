3a054ec031b84f4e413f2d0c8522102622192fba
ajek1993@gmail.com <ajek1993@gmail.com>
2023-09-06 14:40:31 +0200

    basic config

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <label>
+        Name
+        <input />
+      </label>
+      <label>
+        Password
+        <input />
+      </label>
+    </div>
+  );
+}
