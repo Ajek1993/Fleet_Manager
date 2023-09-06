e63b8e2fbea239f6b462c00f4f35fc4efe211fab
ajek1993@gmail.com <ajek1993@gmail.com>
2023-09-06 15:42:04 +0200

    add Authentication

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div>
+      <p>Zaloguj się</p>
+      <label>
+        Name
+        <input />
+      </label>
+      <label>
+        Password
+        <input />
+      </label>
+    </div>
+  );
+}
