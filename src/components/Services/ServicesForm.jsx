ca7fde0075c810e82ec772e860832adcc1660711
ajek1993@gmail.com <ajek1993@gmail.com>
2023-11-02 11:35:25 +0100

    add valitadion to ServiceForm

--- src/components/Services/ServicesForm.jsx
+++ src/components/Services/ServicesForm.jsx
@@ -23,6 +23,15 @@ export default function ServicesForm({ handleFormOpen }) {
     dateOfService: today,
   });
 
+  const [errors, setErrors] = useState({
+    carPlate: "",
+    name: "",
+    costNetto: "",
+    costBrutto: "",
+    invoiceNumber: "",
+    dateOfService: "",
+  });
+
   const handleChange = ({ target: { name, value } }) => {
     setService((prev) => ({
       ...prev,
@@ -32,6 +41,118 @@ export default function ServicesForm({ handleFormOpen }) {
 
   const handleAddService = async (e) => {
     e.preventDefault();
+
+    let errorsCounter = 0;
+
+    //Validation
+
+    if (!service.carPlate) {
+      errorsCounter++;
+      setErrors((prev) => ({
+        ...prev,
+        carPlate: 1,
+      }));
+    } else {
+      setErrors((prev) => ({
+        ...prev,
+        carPlate: "",
+      }));
+    }
+
+    if (!service.name || service.name.length > 100 || service.name.length < 5) {
+      errorsCounter++;
+      setErrors((prev) => ({
+        ...prev,
+        name: 1,
+      }));
+    } else {
+      setErrors((prev) => ({
+        ...prev,
+        name: "",
+      }));
+    }
+
+    if (
+      !service.costNetto ||
+      service.costNetto < 0 ||
+      service.costNetto > 99999 ||
+      !Number.isInteger(+service.costNetto)
+    ) {
+      errorsCounter++;
+      setErrors((prev) => ({
+        ...prev,
+        costNetto: 1,
+      }));
+    } else {
+      setErrors((prev) => ({
+        ...prev,
+        costNetto: "",
+      }));
+    }
+
+    if (
+      !service.costNetto ||
+      service.costNetto < 0 ||
+      service.costNetto > 99999 ||
+      !Number.isInteger(+service.costNetto)
+    ) {
+      errorsCounter++;
+      setErrors((prev) => ({
+        ...prev,
+        costNetto: 1,
+      }));
+    } else {
+      setErrors((prev) => ({
+        ...prev,
+        costNetto: "",
+      }));
+    }
+
+    if (
+      !service.costBrutto ||
+      service.costBrutto < 0 ||
+      service.costBrutto > 99999 ||
+      !Number.isInteger(+service.costBrutto)
+    ) {
+      errorsCounter++;
+      setErrors((prev) => ({
+        ...prev,
+        costBrutto: 1,
+      }));
+    } else {
+      setErrors((prev) => ({
+        ...prev,
+        costBrutto: "",
+      }));
+    }
+
+    if (!service.invoiceNumber || service.invoiceNumber.length > 30) {
+      errorsCounter++;
+      setErrors((prev) => ({
+        ...prev,
+        invoiceNumber: 1,
+      }));
+    } else {
+      setErrors((prev) => ({
+        ...prev,
+        invoiceNumber: "",
+      }));
+    }
+
+    if (!service.dateOfService) {
+      errorsCounter++;
+      setErrors((prev) => ({
+        ...prev,
+        dateOfService: 1,
+      }));
+    } else {
+      setErrors((prev) => ({
+        ...prev,
+        dateOfService: "",
+      }));
+    }
+
+    if (errorsCounter > 0) return;
 
     const docRef = await addDoc(collection(db, "services"), {
       carPlate: service.carPlate,
@@ -73,71 +194,163 @@ export default function ServicesForm({ handleFormOpen }) {
             <InputLabel id="demo-simple-select-label">
               Nr rejestracyjny
             </InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              name="carPlate"
-              value={service.carPlate}
-              label="Nr rejestracyjny"
-              onChange={handleChange}
-            >
-              {carsPlates.map((plate) => (
-                <MenuItem key={plate} value={plate}>
-                  {plate}
-                </MenuItem>
-              ))}
-            </Select>
+            {!errors.carPlate ? (
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                name="carPlate"
+                value={service.carPlate}
+                label="Nr rejestracyjny"
+                onChange={handleChange}
+              >
+                {carsPlates.map((plate) => (
+                  <MenuItem key={plate} value={plate}>
+                    {plate}
+                  </MenuItem>
+                ))}
+              </Select>
+            ) : (
+              <Select
+                error
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                name="carPlate"
+                value={service.carPlate}
+                label="Nr rejestracyjny"
+                onChange={handleChange}
+              >
+                {carsPlates.map((plate) => (
+                  <MenuItem key={plate} value={plate}>
+                    {plate}
+                  </MenuItem>
+                ))}
+              </Select>
+            )}
           </FormControl>
         </Box>
-        <TextField
-          required
-          label="Zakres naprawy"
-          variant="standard"
-          value={service.name}
-          name="name"
-          type="text"
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          label="Koszt netto"
-          variant="standard"
-          value={service.costNetto}
-          name="costNetto"
-          type="number"
-          min="1"
-          max="99999"
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          label="Koszt brutto"
-          variant="standard"
-          value={service.costBrutto}
-          name="costBrutto"
-          type="number"
-          min="1"
-          max="99999"
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          label="Numer faktury"
-          variant="standard"
-          value={service.invoiceNumber}
-          name="invoiceNumber"
-          type="text"
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          label="Data wykonania usługi"
-          variant="standard"
-          value={service.dateOfService}
-          name="dateOfService"
-          type="date"
-          onChange={handleChange}
-        />
+        {!errors.name ? (
+          <TextField
+            required
+            label="Zakres naprawy"
+            variant="standard"
+            value={service.name}
+            name="name"
+            type="text"
+            onChange={handleChange}
+          />
+        ) : (
+          <TextField
+            error
+            helperText="Zakres naprawy powinien składać się z 5-100 znaków"
+            required
+            label="Zakres naprawy"
+            variant="standard"
+            value={service.name}
+            name="name"
+            type="text"
+            onChange={handleChange}
+          />
+        )}
+        {!errors.costNetto ? (
+          <TextField
+            required
+            label="Koszt netto"
+            variant="standard"
+            value={service.costNetto}
+            name="costNetto"
+            type="number"
+            min="1"
+            max="99999"
+            onChange={handleChange}
+          />
+        ) : (
+          <TextField
+            error
+            helperText="Wartość powinna być liczbą z zakresu 0-99999"
+            required
+            label="Koszt netto"
+            variant="standard"
+            value={service.costNetto}
+            name="costNetto"
+            type="number"
+            min="1"
+            max="99999"
+            onChange={handleChange}
+          />
+        )}
+        {!errors.costBrutto ? (
+          <TextField
+            required
+            label="Koszt brutto"
+            variant="standard"
+            value={service.costBrutto}
+            name="costBrutto"
+            type="number"
+            min="1"
+            max="99999"
+            onChange={handleChange}
+          />
+        ) : (
+          <TextField
+            error
+            helperText="Wartość powinna być liczbą z zakresu 0-99999"
+            required
+            label="Koszt brutto"
+            variant="standard"
+            value={service.costBrutto}
+            name="costBrutto"
+            type="number"
+            min="1"
+            max="99999"
+            onChange={handleChange}
+          />
+        )}
+        {!errors.invoiceNumber ? (
+          <TextField
+            required
+            label="Numer faktury"
+            variant="standard"
+            value={service.invoiceNumber}
+            name="invoiceNumber"
+            type="text"
+            onChange={handleChange}
+          />
+        ) : (
+          <TextField
+            error
+            helperText="Numer faktury powinien składać się z max. 30 znaków"
+            required
+            label="Numer faktury"
+            variant="standard"
+            value={service.invoiceNumber}
+            name="invoiceNumber"
+            type="text"
+            onChange={handleChange}
+          />
+        )}
+        {!errors.dateOfService ? (
+          <TextField
+            required
+            label="Data wykonania usługi"
+            variant="standard"
+            value={service.dateOfService}
+            name="dateOfService"
+            type="date"
+            onChange={handleChange}
+          />
+        ) : (
+          <TextField
+            error
+            helperText="Błędna data"
+            required
+            label="Data wykonania usługi"
+            variant="standard"
+            value={service.dateOfService}
+            name="dateOfService"
+            type="date"
+            onChange={handleChange}
+          />
+        )}
 
         <Button
           type="submit"
