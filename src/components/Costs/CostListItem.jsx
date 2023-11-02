31acccc1a59740f21eb5cdc399e23cb9b9a6560a
ajek1993@gmail.com <ajek1993@gmail.com>
2023-11-02 19:29:37 +0100

    fix cost summary table

--- src/components/Costs/CostListItem.jsx
+++ src/components/Costs/CostListItem.jsx
@@ -1,88 +1,114 @@
 import React from "react";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
-import { useUser } from "../../providers/UserProvider";
 
-function CostListItemDetail({ fuel }) {
-  const { services } = useUser();
-
-  const costSum = services
-    .filter(
-      (service) =>
-        service.carPlate === fuel.plate &&
-        +service.dateOfService.slice(0, 4) === fuel.year &&
-        +service.dateOfService.slice(5, 7) === fuel.monthNum
-    )
-    .map((service) => service.costNetto)
-    .reduce((a, c) => +a + +c, 0);
-
+function CostListItemDetail({ cost }) {
   return (
     <TableRow sx={{ "& > td": { textAlign: "center", padding: "10px 0" } }}>
-      <TableCell>{fuel.plate}</TableCell>
-      <TableCell>{costSum} zł</TableCell>
-      <TableCell>{(+fuel.costLPG + +fuel.costPB).toFixed(2)} zł</TableCell>
+      <TableCell>{cost.plate}</TableCell>
+      <TableCell>{cost.services} zł</TableCell>
+      <TableCell>{cost.fuel} zł</TableCell>
       <TableCell>
-        {((+fuel.costLPG + +fuel.costPB + +costSum) / +fuel.distance).toFixed(
-          2
-        )}{" "}
-        zł
+        {!cost.distance
+          ? "-"
+          : ((+cost.fuel + +cost.services) / +cost.distance).toFixed(2) + " zł"}
       </TableCell>
     </TableRow>
   );
 }
 
-export default function CostListItem({ fuel, dateChosen }) {
-  if (
-    dateChosen.plate === "(wszystkie)" &&
-    dateChosen.month === "(wszystkie)" &&
-    dateChosen.year === "(wszystkie)"
-  ) {
-    return <CostListItemDetail key={fuel.ID} fuel={fuel} />;
-  } else if (
-    dateChosen.plate === fuel.plate &&
-    dateChosen.month === "(wszystkie)" &&
-    dateChosen.year === "(wszystkie)"
-  ) {
-    return <CostListItemDetail key={fuel.ID} fuel={fuel} />;
-  } else if (
-    dateChosen.plate === "(wszystkie)" &&
-    dateChosen.month === fuel.month &&
-    dateChosen.year === "(wszystkie)"
-  ) {
-    return <CostListItemDetail key={fuel.ID} fuel={fuel} />;
-  } else if (
-    dateChosen.plate === "(wszystkie)" &&
-    dateChosen.month === "(wszystkie)" &&
-    dateChosen.year === fuel.year
-  ) {
-    return <CostListItemDetail key={fuel.ID} fuel={fuel} />;
-  } else if (
-    dateChosen.plate === fuel.plate &&
-    dateChosen.month === fuel.month &&
-    dateChosen.year === "(wszystkie)"
-  ) {
-    return <CostListItemDetail key={fuel.ID} fuel={fuel} />;
-  } else if (
-    dateChosen.plate === fuel.plate &&
-    dateChosen.month === "(wszystkie)" &&
-    dateChosen.year === fuel.year
-  ) {
-    return <CostListItemDetail key={fuel.ID} fuel={fuel} />;
-  } else if (
-    dateChosen.plate === "(wszystkie)" &&
-    dateChosen.month === fuel.month &&
-    dateChosen.year === fuel.year
-  ) {
-    return <CostListItemDetail key={fuel.ID} fuel={fuel} />;
-  } else {
-    return (
-      <>
-        {fuel.plate === dateChosen.plate &&
-          fuel.month === dateChosen.month &&
-          fuel.year === dateChosen.year && (
-            <CostListItemDetail key={fuel.ID} fuel={fuel} />
-          )}
-      </>
-    );
+export default function CostListItem({ cost, dateChosen }) {
+  if (cost !== undefined && (cost.services || cost.fuel)) {
+    if (
+      dateChosen.plate === "(wszystkie)" &&
+      dateChosen.month === "(wszystkie)" &&
+      dateChosen.year === "(wszystkie)"
+    ) {
+      return (
+        <CostListItemDetail
+          key={cost.plate + cost.year + cost.month}
+          cost={cost}
+        />
+      );
+    } else if (
+      dateChosen.plate === cost.plate &&
+      dateChosen.month === "(wszystkie)" &&
+      dateChosen.year === "(wszystkie)"
+    ) {
+      return (
+        <CostListItemDetail
+          key={cost.plate + cost.year + cost.month}
+          cost={cost}
+        />
+      );
+    } else if (
+      dateChosen.plate === "(wszystkie)" &&
+      dateChosen.month === cost.month &&
+      dateChosen.year === "(wszystkie)"
+    ) {
+      return (
+        <CostListItemDetail
+          key={cost.plate + cost.year + cost.month}
+          cost={cost}
+        />
+      );
+    } else if (
+      dateChosen.plate === "(wszystkie)" &&
+      dateChosen.month === "(wszystkie)" &&
+      dateChosen.year === cost.year
+    ) {
+      return (
+        <CostListItemDetail
+          key={cost.plate + cost.year + cost.month}
+          cost={cost}
+        />
+      );
+    } else if (
+      dateChosen.plate === cost.plate &&
+      dateChosen.month === cost.month &&
+      dateChosen.year === "(wszystkie)"
+    ) {
+      return (
+        <CostListItemDetail
+          key={cost.plate + cost.year + cost.month}
+          cost={cost}
+        />
+      );
+    } else if (
+      dateChosen.plate === cost.plate &&
+      dateChosen.month === "(wszystkie)" &&
+      dateChosen.year === cost.year
+    ) {
+      return (
+        <CostListItemDetail
+          key={cost.plate + cost.year + cost.month}
+          cost={cost}
+        />
+      );
+    } else if (
+      dateChosen.plate === "(wszystkie)" &&
+      dateChosen.month === cost.month &&
+      dateChosen.year === cost.year
+    ) {
+      return (
+        <CostListItemDetail
+          key={cost.plate + cost.year + cost.month}
+          cost={cost}
+        />
+      );
+    } else {
+      return (
+        <>
+          {cost.plate === dateChosen.plate &&
+            cost.month === dateChosen.month &&
+            cost.year === dateChosen.year && (
+              <CostListItemDetail
+                key={cost.plate + cost.year + cost.month}
+                cost={cost}
+              />
+            )}
+        </>
+      );
+    }
   }
 }
